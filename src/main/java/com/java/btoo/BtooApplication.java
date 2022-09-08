03d97d00584b54d52328ca0a31c84671d218cbb6
조수현 <soohyuneeee@josuhyeon-ui-MacBookPro.local>
2022-09-08 09:58:05 +0900

    setting

---
+++ src/main/java/com/java/btoo/BtooApplication.java
@@ -0,0 +1,13 @@
+package com.java.btoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/btoo/BtooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.btoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
