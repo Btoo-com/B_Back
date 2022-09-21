4ac492f470ab42cabb2e0aeefee14bdac204d168
조수현 <soohyuneeee@josuhyeon-ui-MacBookPro.local>
2022-09-21 17:34:52 +0900

    Feat - join gameroom

--- src/main/java/com/java/btoo/Controller/GameroomUserController.java
+++ src/main/java/com/java/btoo/Controller/GameroomUserController.java
@@ -1,13 +1,23 @@
 package com.java.btoo.Controller;
 
+import com.java.btoo.Domain.Gameroom;
+import com.java.btoo.Domain.GameroomUser;
 import com.java.btoo.Repository.GameroomUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @CrossOrigin
 @Controller
 public class GameroomUserController {
     @Autowired
     private GameroomUserRepository gameroomUserRepository;
-}
+
+    @RequestMapping(value = "/joinroom", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+    public String joinRoom(@ModelAttribute GameroomUser gameroomUser)throws Exception {
+        return "success";
+        }
+    }
--- src/main/java/com/java/btoo/Repository/GameroomUserRepository.java
+++ src/main/java/com/java/btoo/Repository/GameroomUserRepository.java
@@ -1,9 +1,13 @@
 package com.java.btoo.Repository;
 
+import com.java.btoo.Domain.GameroomUser;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Mapper
 public interface GameroomUserRepository {
+    @Insert("insert into Gameroom_user values(#{user_id},#{room_id})")
+    void joinRoom(GameroomUser gameroomUser);
 }
